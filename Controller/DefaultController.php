c422240bef41dd8b7fb1f039f76a2f9c2becfe5a
Dariusz Wrzesien <dwrzesien@future-processing.com>
2017-02-23 13:12:43 +0100

    added OpenWeather service

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -13,6 +13,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $openWeather = $this->get('dwr_open_weather');
+        $openWeather->setType('Weather');
 
         return $this->render('DwrOpenWeatherBundle:Default:index.html.twig', array(
             'weather' => $openWeather->getByCityName('London')
---
+++ Service/OpenWeather.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Dwr\OpenWeatherBundle\Service;
+
+class OpenWeather
+{
+    const DEFAULT_TYPE = 'Weather';
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var array
+     */
+    private $supportedType = ['Weather', 'Forecast'];
+
+    /**
+     * OpenWeather constructor.
+     * @param Configuration $config
+     */
+    public function __construct(Configuration $config)
+    {
+        $this->type = self::DEFAULT_TYPE;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        if (! $this->isType($type)) {
+            throw new \InvalidArgumentException(
+                'Unknown OpenWeather type. Supported types are: ' . implode(', ', $this->supportedType)
+            );
+        }
+
+        $this->type = $type;
+    }
+
+    /**
+     * @param string $type
+     * @return bool
+     */
+    private function isType($type)
+    {
+        if (isset($type)
+            && ! empty($type)
+            && in_array($type, $this->supportedType)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSupportedType()
+    {
+        return $this->supportedType;
+    }
+}
